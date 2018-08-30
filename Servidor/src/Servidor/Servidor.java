7a45d9a3b2491bbef76844ff017f9b11a3fb5aa6
erickmoraes_rv@hotmail.com <erickmoraes_rv@hotmail.com>
2018-08-29 22:22:06 -0300

    Servidor

---
+++ Servidor/src/Servidor/Servidor.java
@@ -0,0 +1,33 @@
+
+package Servidor;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Servidor {
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(3312);
+        new Thread(new Runnable() {
+            @Override
+            public void run() { 
+                while (true){
+                    try {
+                        Socket client = server.accept();
+                        if (client != null) {
+                            new Sala(client);
+                        }
+                    } catch (IOException ex) {
+                        Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }   
+            }
+        });
+    }
+}   
+    
+
